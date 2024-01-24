8b6804af47f1495cb5ea8be1d0beb2b793466f48
Kaillr <kaillerplayz@gmail.com>
2024-01-24 18:43:38 +0100

    imported sop! website

---
+++ js/script.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const logoImg = document.querySelector('.logo-img');
+
+    if (logoImg) {
+        const originalSrc = logoImg.src;
+        const alternativeSrc = logoImg.getAttribute('data-alt-src');
+
+        logoImg.addEventListener('mouseover', function () {
+            logoImg.src = alternativeSrc;
+        });
+
+        logoImg.addEventListener('mouseout', function () {
+            logoImg.src = originalSrc;
+        });
+    }
+});
+
+const hamburger = document.querySelector(".hamburger")
+const navMenu = document.querySelector(".nav-menu")
+
+hamburger.addEventListener("click", () =>{
+    hamburger.classList.toggle("active")
+    navMenu.classList.toggle("active")
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n. addEventListener("click", () => {
+    hamburger.classList.remove("active")
+    navMenu.classList.remove("active")
+}))
