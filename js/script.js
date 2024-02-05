f6598e3325fee9797f554f4dd498d9dd0310bb0e
Kaillr <kaillerplayz@gmail.com>
2024-02-05 15:10:36 +0100

    Finished design of FAQs page

--- js/script.js
+++ js/script.js
@@ -27,3 +27,11 @@ document.querySelectorAll(".nav-link").forEach(n => n. addEventListener("click",
     hamburger.classList.remove("active")
     navMenu.classList.remove("active")
 }))
+
+const faqs = document.querySelectorAll(".faqs");
+
+faqs.forEach(faqs => {
+    faqs.addEventListener("click", () =>{
+        faqs.classList.toggle("active");
+    })
+})
