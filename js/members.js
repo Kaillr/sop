984c433ea44c4283de96c56459ff9909e41973bf
Kaillr <kaillerplayz@gmail.com>
2024-05-11 17:34:45 +0200

    Updated members list and replaced the flag assets
    - Added a new member
    - Moved member to right letter
    - Letter "X" was accidentally set as "J" in members list. this was fixed
    - Changed all the flag assets to the flags used by osu
    - Created a fallback flag script in members.js

--- js/members.js
+++ js/members.js
@@ -4,3 +4,28 @@ document.addEventListener("DOMContentLoaded", function() {
         notice.classList.add('active');
     }, 100);
 });
+
+
+
+// Get all flag elements
+const flags = document.querySelectorAll('.flag');
+
+// Loop through each flag element
+flags.forEach(flag => {
+    // Get the flag source
+    const flagSrc = flag.getAttribute('src');
+    
+    // Check if the flag source exists
+    fetch(flagSrc)
+        .then(response => {
+            // If the flag exists, do nothing
+            if (!response.ok) {
+                // If the flag does not exist, set the fallback flag source
+                flag.setAttribute('src', '/media/images/flags/__.png');
+            }
+        })
+        .catch(error => {
+            // If there's an error, set the fallback flag source
+            flag.setAttribute('src', '/media/images/flags/__.png');
+        });
+});
