885d839eaa041512d3523a8f3557e28a47114603
Kaillr <kaillerplayz@gmail.com>
2024-05-20 22:03:16 +0200

    Update members.js

--- js/members.js
+++ js/members.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('https://https://shitosuplayers.xyz/test-api')
+    fetch('https://84.210.67.128//test-api')
         .then(response => response.json())
         .then(data => {
             console.log('Fetched osu! API data:', data);
