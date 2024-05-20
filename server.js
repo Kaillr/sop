d0a2b39e66228611fedd7da0c581b9ef72eb2935
Kaillr <kaillerplayz@gmail.com>
2024-05-20 22:21:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-// Server side
 const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
-const apiKey = '54340cac03339a8902a238ca2831b37762067fb3'; // Replace with your API key
+const PORT = process.env.PORT || 3000;
 
 // Middleware to handle CORS
 app.use((req, res, next) => {
@@ -10,29 +9,23 @@ app.use((req, res, next) => {
     next();
 });
 
-// Route to fetch member data
+// Route to test osu! API
 app.get('/test-api', async (req, res) => {
-    try {
-        // Fetch data from the osu! API
-        const apiUrl = `https://osu.ppy.sh/api/get_user?k=${apiKey}&u=xiraphotography`;
-        const response = await fetch(apiUrl);
-        
-        // Log the raw response content
-        const responseData = await response.text();
-        console.log('Response from osu! API:', responseData);
+    const apiKey = '54340cac03339a8902a238ca2831b37762067fb3'; // Replace with your API key
+    const osuUserId = '15794645'; // Replace with a valid osu! user ID
+    const apiUrl = `https://osu.ppy.sh/api/get_user?k=${apiKey}&u=${osuUserId}`;
 
-        // Parse the response as JSON
-        const userData = JSON.parse(responseData);
-        
-        // Send the parsed JSON response to the client
-        res.json(userData);
+    try {
+        const response = await fetch(apiUrl);
+        const responseData = await response.json();
+        console.log('Response from osu! API:', responseData);
+        res.json(responseData);
     } catch (error) {
         console.error('Error fetching osu! API data:', error);
         res.status(500).json({ error: 'Internal server error' });
     }
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
