c593843792d2578de8bc4358de151e922be2f905
Kaillr <kaillerplayz@gmail.com>
2024-07-24 19:51:07 +0200

    Updated faq toggle

--- frontend/js/faqs.js
+++ frontend/js/faqs.js
@@ -4,7 +4,12 @@ const questions = document.querySelectorAll('.question');
 // Loop through each question element and add a click event listener
 questions.forEach(question => {
     question.addEventListener('click', () => {
-        // Toggle the 'active' class on the parent 'faqs' element
+        // Remove the 'active' class from all '.faqs' elements
+        document.querySelectorAll('.faqs.active').forEach(faq => {
+            faq.classList.remove('active');
+        });
+
+        // Toggle the 'active' class on the parent '.faqs' element of the clicked question
         question.closest('.faqs').classList.toggle('active');
     });
 });
